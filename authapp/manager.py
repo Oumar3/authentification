bd4bf7bac9f5e272d66e482608d2dbe9b4af9b87
Oumar3 <oumaralitori1@gmail.com>
2024-11-11 22:19:52 +0100

    test ok

--- authapp/manager.py
+++ authapp/manager.py
@@ -23,8 +23,8 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", True)
 
         if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
+            raise ValueError("Superuser is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
+            raise ValueError("Superuser is_superuser=True.")
 
         return self.create_user(email, password, **extra_fields)
--- authapp/models.py
+++ authapp/models.py
@@ -13,7 +13,7 @@ class CustomeUser(AbstractBaseUser, PermissionsMixin):
     An abstract base class implementing a fully featured User model with
     admin-compliant permissions.
 
-    Username and password are required. Other fields are optional.
+    Email and password are required. Other fields are optional.
     """
     first_name = models.CharField(_("first name"), max_length=150, blank=True)
     last_name = models.CharField(_("last name"), max_length=150, blank=True)
@@ -53,21 +53,9 @@ class CustomeUser(AbstractBaseUser, PermissionsMixin):
         verbose_name = _("CustomeUser")
         verbose_name_plural = _("CustomeUsers")
 
-    def clean(self):
-        super().clean()
-        self.email = self.__class__.objects.normalize_email(self.email)
+    
 
-    def get_full_name(self):
-        """
-        Return the first_name plus the last_name, with a space in between.
-        """
-        full_name = "%s %s" % (self.first_name, self.last_name)
-        return full_name.strip()
-
-    def get_short_name(self):
-        """Return the short name for the user."""
-        return self.first_name
-
-    def email_user(self, subject, message, from_email=None, **kwargs):
-        """Send an email to this user."""
-        send_mail(subject, message, from_email, [self.email], **kwargs)
+    # def save(self, *args, **kwargs):
+    #     s
+    #     return super().save(*args, **kwargs)
+        
